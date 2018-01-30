ab11a91003d3111cc27edbac627ef0399f8e4bae
Akhil Mohammed <mohammed.akhil@gyanmatrix.com>
2018-01-30 12:45:00 +0530

    removed cross origin request

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ app.use('/api/recordings',index);
 //app.use(express.static('client/build'));
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://project-zoom.herokuapp.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error('Not Found');
